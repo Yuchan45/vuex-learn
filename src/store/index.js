a4d4a244400609c1d6bf490e2647725321c11510
Yuchan <yu.nakasone@gmail.com>
2023-06-01 23:35:13 -0300

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,52 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		counter: 0,
+		colorCode: 'red'
+	},
+	mutations: {
+		// Aca van los setter para los states. Se dice "commit" a mutation.
+		// Aca no se puede usar codigo asyncrono.
+		increaseCounter(state, randomNumber) {
+			state.counter += randomNumber;
+		},
+		decreaseCounter(state, randomNumber) {
+			state.counter -= randomNumber;
+		},
+		setColorCode(state, newValue) {
+			state.colorCode = newValue;
+		}
+
+
+	},
+	actions: {
+		// Se dice "dispatch" an action.
+		// Son metodos que NO puede cambiar data en el state. Pero puede acceder a esta.
+		// Para realizar un cambio en el state desde aca, hay que usar una mutation.
+		// Puede haber codigo asyncrono.
+		increaseCounter({ commit }) {
+			axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+				commit('increaseCounter', response.data);
+			})
+		},
+		decreaseCounter({ commit }) {
+			axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+				commit('decreaseCounter', response.data);
+			})
+		},
+		setColorCode({ commit }, newValue) {
+			commit('setColorCode', newValue);
+		}
+	},
+	getters: {
+		// permite obtener data de un state.
+		// Sirve para ejecutar alguna logica en el valor obtenido del state antes de devolverlo.
+		counterSquared(state) {
+			return state.counter * state.counter;
+		}
+
+
+	}
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<div :style="{ color: $store.state.colorCode }" class="counter">
+			{{ $store.state.counter }}
+		</div>
+		<div class="counter-squared">
+			{{ $store.state.counter }} 
+			<sup>2</sup> = 
+			{{ $store.getters.counterSquared }}
+		</div>
+		<div class="buttons">
+			<button @click="$store.dispatch('decreaseCounter')">-</button>
+			<button @click="$store.dispatch('increaseCounter')">+</button>
+		</div>
+		<div>
+			<input 
+				placeholder="Enter color code" 
+				type="text"
+				v-model="colorCode"
+			>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// @ is an alias to /src
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'HomeView',
+		computed: {
+			colorCode: {
+				get() {
+					return this.$store.state.colorCode
+				},
+				set(newValue) {
+					this.$store.dispatch('setColorCode', newValue);
+				}
+			}
+		}
+
+	}
 </script>
+
+
+<style>
+	div {
+		margin-bottom: 10px;
+	}
+
+	.counter {
+		font-size: 80px;
+	}
+
+	.buttons button {
+		font-size: 40px;
+		width: 100px;
+		margin: 0 10px;
+	}
+
+
+</style>
